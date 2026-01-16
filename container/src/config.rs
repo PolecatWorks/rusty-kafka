9b215ff6f65c10091b1c25a6b0f7b09511ff953c
Ben Greene <BenJGreene@gmail.com>
2026-01-16 20:38:16 +0000

    Updates to add options for metrics

---
+++ container/src/config.rs
@@ -0,0 +1,62 @@
+use figment::{
+    providers::{Env, Format, Yaml},
+    Figment,
+};
+use figment_file_provider_adapter::FileAdapter;
+use hamsrs::hams::config::HamsConfig;
+use serde::Deserialize;
+use std::path::Path;
+
+use crate::tokio_tools::ThreadRuntime;
+
+#[derive(Deserialize, Debug, Clone, Default)]
+pub struct KafkaConfig {
+    pub brokers: String,
+    pub registry: String,
+    pub num_workers: i32,
+    pub input_topic: String,
+    pub output_topic: String,
+    pub group_id: String,
+}
+
+fn default_name() -> String {
+    crate::NAME.to_string()
+}
+
+fn default_version() -> String {
+    crate::VERSION.to_string()
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct MyConfig {
+    #[serde(default = "default_name")]
+    pub name: String,
+    #[serde(default = "default_version")]
+    pub version: String,
+    /// Config of my web service
+    pub hams: HamsConfig,
+    pub runtime: ThreadRuntime,
+    pub kafka: KafkaConfig,
+}
+
+impl Default for MyConfig {
+    fn default() -> Self {
+        Self {
+            name: default_name(),
+            version: default_version(),
+            hams: HamsConfig::default(),
+            runtime: ThreadRuntime::default(),
+            kafka: KafkaConfig::default(),
+        }
+    }
+}
+
+impl MyConfig {
+    // Note the `nested` option on both `file` providers. This makes each
+    // top-level dictionary act as a profile.
+    pub fn figment<P: AsRef<Path> + Clone>(yaml_string: &str, secrets: P) -> Figment {
+        Figment::new()
+            .merge(FileAdapter::wrap(Yaml::string(yaml_string)).relative_to_dir(secrets))
+            .merge(Env::prefixed("APP_").split("__"))
+    }
+}
---
+++ container/src/old-code/produce.rs
@@ -0,0 +1,57 @@
+use std::error::Error;
+use std::time::Duration;
+
+use apache_avro::types::Record as AvroRecord;
+use apache_avro::{Schema, Writer};
+
+use kafka::producer::{Producer, Record, RequiredAcks};
+// use kafka::utils::Shutdown;
+
+const AVRO_SCHEMA: &str = r#"{
+  "type": "record",
+  "name": "User",
+  "fields": [
+    {"name": "name", "type": "string"},
+    {"name": "favourite_number",  "type": "int"}
+  ]
+}"#;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let broker = "localhost:9092".to_owned();
+
+    // Create a Kafka producer and specify the topic that we want to write messages to.
+    let mut producer = Producer::from_hosts(vec![broker])
+        // .with_ack_timeout(1000)
+        .with_ack_timeout(Duration::from_secs(1))
+        .with_required_acks(RequiredAcks::One)
+        .create()?;
+
+    // Create an Avro writer and specify the Avro schema that we want to use to encode the messages.
+    let schema = Schema::parse_str(AVRO_SCHEMA)?;
+    let mut writer = Writer::new(&schema, Vec::new());
+    // let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
+
+    println!("{:?}", schema);
+
+    let mut record = AvroRecord::new(writer.schema()).unwrap();
+    record.put("name", "Ben");
+    record.put("favourite_number", 3);
+
+    writer.append(record).unwrap();
+
+    let encoded = writer.into_inner().unwrap();
+
+    println!("Encoded = {}", encoded.len());
+
+    // ToDo: Update this to use Confluent compatible Avro : https://docs.confluent.io/3.2.0/schema-registry/docs/serializer-formatter.html#wire-format
+
+    producer.send(&Record {
+        topic: "test.topic",
+        partition: -1,
+        key: (),
+        value: encoded,
+    })?;
+
+    println!("Finished");
+    Ok(())
+}
